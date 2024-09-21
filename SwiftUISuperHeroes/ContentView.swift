ce029238e2c35460ca4bf9e72871f7ae9e076955
Patricia Martinez Espert <patricia.martinez@edreamsodigeo.com>
2024-09-21 21:02:32 +0200

    Initial Commit

---
+++ SwiftUISuperHeroes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUISuperHeroes
+//
+//  Created by patricia.martinez on 21/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUISuperHeroes/SwiftUISuperHeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISuperHeroesApp.swift
+//  SwiftUISuperHeroes
+//
+//  Created by patricia.martinez on 21/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISuperHeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
